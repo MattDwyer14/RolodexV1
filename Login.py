720c5f20c59ac57ad9f4575404e50d17ab049e3c
MattDwyer14 <64387012+MattDwyer14@users.noreply.github.com>
2023-09-27 18:22:45 +0100

    - Verification now passed through running loop to allow progression to home page - aesthetic changes to login page

--- Login.py
+++ Login.py
@@ -1,27 +1,24 @@
 import tkinter as tk
 from tkinter import messagebox
 
-username_entry = None
-password_entry = None
-verified = False
-home = False
-window = None
-
 
 def login_page():
     global username_entry, password_entry, verified, home, window
     window = tk.Tk()
     window.title("Login")
-    window.geometry('550x450')
+    window.geometry('500x400')
     window.configure(bg='#000000')
 
-    frame = tk.Frame(bg='#000000', highlightbackground='green2', highlightthickness=2)
+    frame = tk.Frame(bg='#000000')
     login_label = tk.Label(frame, text="Login", bg='#000000', fg='green2', font=("Arial", 30))
-    username_label = tk.Label(frame, text="Username", bg='#8F00FF', fg="#FFFFFF", font=("Arial", 16, 'bold'))
-    password_label = tk.Label(frame, text="Password", bg='#8F00FF', fg="#FFFFFF", font=("Arial", 16, 'bold'))
-    username_entry = tk.Entry(frame, font=("Arial", 16))
-    password_entry = tk.Entry(frame, show="*", font=("Arial", 16))
-    login_button = tk.Button(frame, text="Login", bg="#DC143C", fg="#FFFFFF", font=("Arial", 16), command=login)
+    username_label = tk.Label(frame, text="Username", bg='#000000', fg="green2", font=("Arial", 16, 'bold'))
+    password_label = tk.Label(frame, text="Password", bg='#000000', fg="green2", font=("Arial", 16, 'bold'))
+    username_entry = tk.Entry(frame, font=("Arial", 16), bg='#000000', fg="green2",
+                              highlightbackground='green2', highlightthickness=2)
+    password_entry = tk.Entry(frame, show="*", font=("Arial", 16), bg='#000000', fg="green2",
+                              highlightbackground='green2', highlightthickness=2)
+    login_button = tk.Button(frame, text="Login", bg="#000000", fg="green2", font=("Arial", 16),
+                             highlightbackground='green2', highlightthickness=3, command=login)
 
     login_label.grid(row=0, column=0, columnspan=2, sticky="news", pady=40)
     username_label.grid(row=1, column=0)
@@ -36,7 +33,6 @@ def login_page():
     return verified
 
 
-
 def login():
     global username_entry, password_entry, verified, home, window
     username = "a"
@@ -46,15 +42,7 @@ def login():
         verified = True
         window.destroy()
         print(verified, "stage 1")
-        #return verified
-
+        return verified
 
     else:
         tk.messagebox.showerror(title="Error", message="Invalid login.")
-        verified = False
-        return verified
-
-
-
-
-
--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-
 from Login import *
 from home import *
+
 verified = False
 home = False
 running = True
+
 while running:
     if not verified:
-        login_page()
+        verified = login_page()
         print(verified, "after")
 
-    print(verified, "outisde")
-    if home:
+    if verified:
         home_page()
 
 
