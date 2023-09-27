417f22de0161fa2723c98dde1b4b61343c6aaa59
MattDwyer14 <64387012+MattDwyer14@users.noreply.github.com>
2023-09-27 19:26:31 +0100

    -added title to homepage -added a canvas to the homepage whose wize varies with the size of the window

--- Login.py
+++ Login.py
@@ -7,20 +7,20 @@ def login_page():
     window = tk.Tk()
     window.title("Login")
     window.geometry('500x400')
-    window.configure(bg='#000000')
+    window.configure(bg='dark slate grey')
 
-    frame = tk.Frame(bg='#000000')
-    login_label = tk.Label(frame, text="Login", bg='#000000', fg='green2', font=("Arial", 30))
-    username_label = tk.Label(frame, text="Username", bg='#000000', fg="green2", font=("Arial", 16, 'bold'))
-    password_label = tk.Label(frame, text="Password", bg='#000000', fg="green2", font=("Arial", 16, 'bold'))
-    username_entry = tk.Entry(frame, font=("Arial", 16), bg='#000000', fg="green2",
-                              highlightbackground='green2', highlightthickness=2)
-    password_entry = tk.Entry(frame, show="*", font=("Arial", 16), bg='#000000', fg="green2",
-                              highlightbackground='green2', highlightthickness=2)
-    login_button = tk.Button(frame, text="Login", bg="#000000", fg="green2", font=("Arial", 16),
-                             highlightbackground='green2', highlightthickness=3, command=login)
+    frame = tk.Frame(bg='dark slate grey')
+    login_label = tk.Label(frame, text="Login", bg='dark slate grey', fg='navajo white', font=("Lucida Sans Typewriter", 30))
+    username_label = tk.Label(frame, text="Username: ", bg='dark slate grey', fg="navajo white", font=("Lucida Sans Typewriter", 16, 'bold'))
+    password_label = tk.Label(frame, text="Password: ", bg='dark slate grey', fg="navajo white", font=("Lucida Sans Typewriter", 16, 'bold'))
+    username_entry = tk.Entry(frame, font=("Lucida Sans Typewriter", 16), bg='dark slate grey', fg="navajo white",
+                              highlightbackground='navajo white', highlightthickness=2)
+    password_entry = tk.Entry(frame, show="*", font=("Lucida Sans Typewriter", 16), bg='dark slate grey',
+                              fg="navajo white", highlightbackground='navajo white', highlightthickness=2)
+    login_button = tk.Button(frame, text="Login", bg="navajo white", fg="dark slate grey",
+                             font=("Lucida Sans Typewriter", 20, 'bold'), highlightbackground='navajo white', highlightthickness=1, command=login)
 
-    login_label.grid(row=0, column=0, columnspan=2, sticky="news", pady=40)
+    login_label.grid(row=0, column=0, columnspan=1, sticky="news", pady=30)
     username_label.grid(row=1, column=0)
     username_entry.grid(row=1, column=1, pady=20)
     password_label.grid(row=2, column=0)
@@ -38,7 +38,6 @@ def login():
     username = "a"
     password = "a"
     if username_entry.get() == username and password_entry.get() == password:
-        tk.messagebox.showinfo(title="Login Successful!", message="You successfully logged in.")
         verified = True
         window.destroy()
         print(verified, "stage 1")
--- home.py
+++ home.py
@@ -1,7 +1,43 @@
 import tkinter as tk
+
+
 def home_page():
-    window = tk.Tk()
-    window.title("home")
-    window.geometry('750x550')
-    window.configure(bg='#000000')
-    window.mainloop()
+    homepage = tk.Tk()
+    homepage.title("Rolodex.V1")
+    homepage.geometry('750x550')
+    homepage.configure(bg='dark slate grey')
+
+    def toggle_fullscreen():
+        state = not homepage.attributes('-fullscreen')
+        homepage.attributes('-fullscreen', state)
+        return "break"
+
+    # Uncomment if you want the fullscreen toggle feature
+    # homepage.attributes('-fullscreen', True)
+    # homepage.bind("<Escape>", toggle_fullscreen)
+
+    # Setting weights for rows and columns
+    homepage.grid_rowconfigure(0, weight=1)  # title's row
+    homepage.grid_rowconfigure(1, weight=5)  # canvas's row
+    homepage.grid_columnconfigure(0, weight=1)  # canvas's column
+
+    title = tk.Label(text="Rolodex.V1", bg='dark slate grey', fg='navajo white', font=("Lucida Sans Typewriter", 30))
+    title.grid(row=0, column=0, sticky="nw")
+
+    # Create a canvas for drawing
+    canvas = tk.Canvas(homepage, bg="blue")
+    canvas.grid(row=1, column=0, sticky="nsew")
+
+    homepage.mainloop()
+
+
+home_page()
+
+
+home_page()
+
+
+
+
+
+
