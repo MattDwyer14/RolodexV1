c1e641bc276f323c2994baefb9301eb4dab99ca0
MattDwyer14 <64387012+MattDwyer14@users.noreply.github.com>
2023-10-10 17:27:27 +0100

    - created subtitle function to add subtitle dependent on which tab is opened -moved all app tile set up into home_setup() to allow for better control over moving between applications and homepage

--- Journal.py
+++ Journal.py
@@ -26,36 +26,13 @@ class app_tile:
         tile.grid(row=self.row, column=self.column)
 
 
-def journal():
-    subtitle = tk.Label(text="Journal", bg='dark slate grey', fg='navajo white', font=("Lucida Sans Typewriter", 15))
-    subtitle.grid(row=0, column=1, sticky="sw")
-
-
-def home_setup():
-    journal_tile = app_tile(0, 0, 10, "Journal", ("Lucida Sans Typewriter", 16, 'bold'), journal, 'dark slate grey',
-                            'navajo white', 'navajo white', 1)
-    journal_tile.create_tile()
-    network_tile = app_tile(1, 0, 10, "Network", ("Lucida Sans Typewriter", 16, 'bold'), None, 'dark slate grey',
-                            'navajo white', 'navajo white', 1)
-    network_tile.create_tile()
-    health_tile = app_tile(2, 0, 10, "Health", ("Lucida Sans Typewriter", 16, 'bold'), None, 'dark slate grey',
-                           'navajo white', 'navajo white', 1)
-    health_tile.create_tile()
-    finance_tile = app_tile(3, 0, 10, "Finance", ("Lucida Sans Typewriter", 16, 'bold'), None, 'dark slate grey',
-                            'navajo white', 'navajo white', 1)
-    finance_tile.create_tile()
-    calender_tile = app_tile(4, 0, 10, "Calender", ("Lucida Sans Typewriter", 16, 'bold'), None, 'dark slate grey',
-                             'navajo white', 'navajo white', 1)
-    calender_tile.create_tile()
-
-
 def update_time():
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     date_time.config(text=current_time)
     window.after(1000, update_time)
 
 
-def home_page():
+def journal_page():
     global date_time, window, frame
 
     window = tk.Tk()
@@ -68,25 +45,25 @@ def home_page():
     logo_resized = logo.resize((65, 65))
     logo_image = ImageTk.PhotoImage(logo_resized)
     logo_label = tk.Label(image=logo_image, highlightthickness=0, highlightbackground='navajo white')
+    logo_label.grid(row=0, column=3, sticky="e")
 
     window.grid_rowconfigure(0, weight=0)  # title's row
     window.grid_rowconfigure(1, weight=1)  # canvas's row
-    window.grid_columnconfigure(0, weight=0)# canvas's column
-    window.grid_columnconfigure(1, weight=1)# canvas's column
+    window.grid_columnconfigure(0, weight=1)  # canvas's column
 
-    title = tk.Label(text="Rolodex.V1", bg='dark slate grey', fg='navajo white', font=("Lucida Sans Typewriter", 40))
+    title = tk.Label(text="RolodexV1", bg='dark slate grey', fg='navajo white', font=("Lucida Sans Typewriter", 40))
     title.grid(row=0, column=0, sticky="sw")
+
+    subtitle = tk.Label(text="Journal", bg='dark slate grey', fg='navajo white', font=("Lucida Sans Typewriter", 20))
+    subtitle.grid(row=0, column=1, sticky="sw")
 
     frame = tk.Frame(window, bg="dark slate grey", highlightbackground='navajo white', highlightthickness=3)
     frame.grid(row=1, column=0, sticky="nsew", columnspan=4)
 
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    date_time = tk.Label(text=current_time, bg='dark slate grey', fg='navajo white',
-                         font=("Lucida Sans Typewriter", 15))
-    date_time.grid(row=0, column=2, sticky='se', padx=10)
-    logo_label.grid(row=0, column=3, sticky="e")
+    date_time = tk.Label(text=current_time, bg='dark slate grey', fg='navajo white', font=("Arial", 16))
+    date_time.grid(row=0, column=2, sticky='sw', padx=30)
 
-    home_setup()
 
     frame.grid_rowconfigure(1, weight=0)  # edit weights once rest of page design is finished
     frame.grid_columnconfigure(0, weight=0)
@@ -95,5 +72,4 @@ def home_page():
 
     window.mainloop()
 
-
-home_page()
+window()
