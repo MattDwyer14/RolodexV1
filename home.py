894f0e45d2ea98a2df3283a2d4f70bbe92674820
MattDwyer14 <64387012+MattDwyer14@users.noreply.github.com>
2023-10-10 16:46:23 +0100

    - created app_tile class to create clickable tiles for access to all Rolodex applications

--- home.py
+++ home.py
@@ -3,15 +3,36 @@ from datetime import datetime
 from PIL import ImageTk,Image
 
 
+class app_tile:
+
+    def __init__(self, row, column, width, text, font, function, background_colour, text_colour, highlight_background,
+                 highlight_thickness):
+
+        self.row = row
+        self.column = column
+        self.width = width
+        self.text = text
+        self.font = font
+        self.function = function
+        self.background = background_colour
+        self.text_colour = text_colour
+        self.highlight_background = highlight_background
+        self.highlight_thickness = highlight_thickness
+
+    def create_tile(self):
+        tile = tk.Button(frame, text=self.text, bg=self.background, fg=self.text_colour, font=self.font,
+                         highlightbackground=self.highlight_background, highlightthickness=self.highlight_thickness
+                         , command=self.function, width=self.width)
+        tile.grid(row=self.row, column=self.column)
+
 def update_time():
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     date_time.config(text=current_time)
-    print("time")
     homepage.after(1000, update_time)
 
 
 def home_page():
-    global date_time, homepage
+    global date_time, homepage, frame
 
     homepage = tk.Tk()
     homepage.title("Rolodex.V1")
@@ -38,8 +59,18 @@ def home_page():
     date_time.grid(row=0, column=1, sticky='sw',padx=30)
     logo_label.grid(row=0, column=2, sticky="e")
 
+    journal_tile = app_tile(0, 0, 10, "Journal", ("Lucida Sans Typewriter", 16, 'bold'), None, 'dark slate grey',
+                         'navajo white', 'navajo white', 1)
+    journal_tile.create_tile()
+    network_tile = app_tile(1, 0, 10, "Network", ("Lucida Sans Typewriter", 16, 'bold'), None, 'dark slate grey',
+                         'navajo white', 'navajo white', 1)
+    network_tile.create_tile()
+    Health_tile = app_tile(2, 0, 10, "Health", ("Lucida Sans Typewriter", 16, 'bold'), None, 'dark slate grey',
+                            'navajo white', 'navajo white', 1)
+    Health_tile.create_tile()
+
     update_time()
 
     homepage.mainloop()
 
-#home_page()
+home_page()
