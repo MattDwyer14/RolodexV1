bf2d2d27f77d2786720f4dbeacbe6a3d369de42a
MattDwyer14 <64387012+MattDwyer14@users.noreply.github.com>
2023-10-16 18:07:42 +0100

    added connection to database using config.json. also defined some functions that will be implemented in the program

--- journal_functions.py
+++ journal_functions.py
@@ -1,2 +1,18 @@
-import mysql
+import json
+import mysql.connector
 
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+db = mysql.connector.connect(
+    host=config['host'],
+    user=config['user'],
+    password=config['password'],
+    database=config['database']
+)
+
+def fetch_entries():
+    pass
+
+def add_entry():
+    pass
